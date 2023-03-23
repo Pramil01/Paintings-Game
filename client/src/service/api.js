d1f36739ec0f789867d4c26b9d92f4c975496739
Pramil Kesarwani <kesarwanipramil01@gmail.com>
2023-03-23 17:44:21 +0530

    api changed

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let baseURL = "http://localhost:5000/";
+let baseURL = "https://painting-game.onrender.com/";
 // if (process.env.NODE_ENV === "production") {
 //   baseURL = "https://App-name.herokuapp.com";
 // } else {
