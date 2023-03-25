836f807f42b0c37e28baaaf9bd9828ff609f96ec
Pramil Kesarwani <kesarwanipramil01@gmail.com>
2023-03-25 19:08:02 +0530

    backend url changed

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let baseURL = "https://painting-game.onrender.com/";
+let baseURL = "https://13-233-198-227.nip.io:5443/";
 // if (process.env.NODE_ENV === "production") {
 //   baseURL = "https://App-name.herokuapp.com";
 // } else {
